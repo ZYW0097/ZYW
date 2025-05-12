54cec9df9e2a88d6bf11080a907fabd68dde8cf5
ZYW0097 <zyw097@gmail.com>
2025-05-12 23:52:25 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const connectDB = require('./config/database');
+require('dotenv').config();
+
+const app = express();
+
+// 連接數據庫
+connectDB();
+
+// 設置視圖引擎
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// 中間件
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Session 設置
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+// 路由
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// 啟動服務器
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`服務器運行在端口 ${PORT}`);
+});
